1fde253cb59b0537c0f0582461ea4c6c27aba142
Sergio Pereira <sergiohgp@gmail.com>
2020-10-10 14:53:28 -0400

    Input and Button started

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
-
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
   return (
@@ -11,6 +13,13 @@ const SignIn: React.FC = () => {
       <Image source={logoImg} />
 
       <Title >Log In</Title>
+
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="Password" />
+
+      <Button onPress={() => {
+        console.log("Work");
+      }}>Enter</Button>
     </Container>
 
   );
